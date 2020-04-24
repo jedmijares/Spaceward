69271fc1c210f81da970b812815def1aa7d4a4db
Jed <jamijares3@gmail.com>
2020-04-24 16:00:00 -0500

    implements crosshair
    https://opengameart.org/content/3-fps-crosshairs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // https://docs.unity3d.com/ScriptReference/Cursor.SetCursor.html
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnMouseEnter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
