883dbf5d90f69392d419bc81ea5c97c8f5f11ed0
Jed <jamijares3@gmail.com>
2020-04-25 10:20:10 -0500

    randomizes spawn position

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -11,11 +11,16 @@ public class Spawner : MonoBehaviour
     public Vector3 spawnPosition;
     public float SpawnTime = 5.0f;
     public float InitialTime;
+    public float OffscreenOffset = 10;
+    public float ZPosition = 40;
+
+    private Camera cam;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("Spawn", InitialTime, SpawnTime);
+        cam = Camera.main;
     }
 
     // Update is called once per frame
@@ -28,6 +33,39 @@ public class Spawner : MonoBehaviour
     void Spawn()
     {
         GameObject spawned = Instantiate(objects);
-        spawned.transform.position = spawnPosition;
+        spawned.transform.position = getPosOffscreen(OffscreenOffset,ZPosition);
+    }
+
+    // at the given z value, generate a Vector3 representing a random position a distance offset offscreen
+    Vector3 getPosOffscreen(float offset, float z)
+    {
+        Vector3 position = Vector3.zero;
+        position.z = z;
+        // Vector3 destination = cam.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), z));
+        if((Random.value > 0.5f))
+        {
+            position.y = Random.Range(0, cam.pixelHeight);
+            if ((Random.value > 0.5f))
+            {
+                position.x = cam.pixelWidth + offset;
+            }
+            else
+            {
+                position.x = 0 - offset;
+            }
+        }
+        else
+        {
+            position.x = Random.Range(0, cam.pixelWidth);
+            if ((Random.value > 0.5f))
+            {
+                position.y = cam.pixelHeight + offset;
+            }
+            else
+            {
+                position.y = 0 - offset;
+            }
+        }
+        return cam.ScreenToWorldPoint(position);
     }
 }
