d90380e8cecabeb9d8375953e62997cf7407ebbb
Jed <jamijares3@gmail.com>
2020-05-18 21:05:00 -0500

    implements shootingenemy as an extension of enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,66 +8,40 @@ public class Enemy : SpawnableObject
     public int curHp;
     public int maxHp = 50;
     public int scoreToGive = 50;
-    public int minShotVolley = 3;
-    public int maxShotVolley = 5;
+    //public int minShotVolley = 3;
+    //public int maxShotVolley = 5;
 
     [Header("Movement")]
     public float moveSpeed = 15;
     public int screenEdgeOffset = 75;
     public int distanceFromCamera = 40;
 
-    private Weapon weapon;
-    private Vector3 destination; // next position to move towards
-    private Camera cam;
-    private bool shooting;
-    private int shotVolley;
+    //private Weapon weapon;
+    protected Vector3 destination; // next position to move towards
+    protected Camera cam;
+    //private bool shooting;
+    //private int shotVolley;
 
 
-    void Start ()
-    {
-        // get the components
-        weapon = GetComponent<Weapon>();
-        cam = Camera.main;
-        destination = cam.ScreenToWorldPoint(new Vector3(Random.Range(screenEdgeOffset, Screen.width - screenEdgeOffset), Random.Range(screenEdgeOffset, Screen.height - screenEdgeOffset), distanceFromCamera));
-    }
+    //void Start ()
+    //{
+    //    // get the components
+        
+        
+        
+    //}
 
-    private void OnEnable()
-    {
-        curHp = maxHp;
-        shooting = false;
-        shotVolley = Random.Range(minShotVolley, maxShotVolley + 1);
-    }
+    //private void OnEnable()
+    //{
+        
+    //    //shooting = false;
+    //    //shotVolley = Random.Range(minShotVolley, maxShotVolley + 1);
+    //}
 
-    void Update ()
-    {
-        if (shooting)
-        {
-            if (weapon.CanShoot())
-            {
-                weapon.Shoot();
-                shotVolley--;
-            }
-            if (shotVolley == 0)
-            {
-                shooting = false;
-                shotVolley = Random.Range(minShotVolley, maxShotVolley);
-            }
-        }
-        else
-        {
-            Move();
-        }
-    }
-
-    void Move()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
-        if (transform.position == destination)
-        {
-            destination = cam.ScreenToWorldPoint(new Vector3(Random.Range(screenEdgeOffset, Screen.width - screenEdgeOffset), Random.Range(screenEdgeOffset, Screen.height - screenEdgeOffset), distanceFromCamera));
-            shooting = true;
-        }
-    }
+    //void Update ()
+    //{
+        
+    //}
 
     public void TakeDamage (int damage)
     {
---
+++ Assets/Scripts/ShootingEnemy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemy : Enemy
+{
+    [Header("Stats")]
+    public int minShotVolley = 3;
+    public int maxShotVolley = 5;
+
+    private Weapon weapon;
+    //private Vector3 destination; // next position to move towards
+    private bool shooting;
+    private int shotVolley;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        weapon = GetComponent<Weapon>();
+        destination = cam.ScreenToWorldPoint(new Vector3(Random.Range(screenEdgeOffset, Screen.width - screenEdgeOffset), Random.Range(screenEdgeOffset, Screen.height - screenEdgeOffset), distanceFromCamera));
+    }
+
+    private void OnEnable()
+    {
+        //curHp = maxHp;
+        curHp = maxHp;
+        shooting = false;
+        shotVolley = Random.Range(minShotVolley, maxShotVolley + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shooting)
+        {
+            if (weapon.CanShoot())
+            {
+                weapon.Shoot();
+                shotVolley--;
+            }
+            if (shotVolley == 0)
+            {
+                shooting = false;
+                shotVolley = Random.Range(minShotVolley, maxShotVolley);
+            }
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
+        if (transform.position == destination)
+        {
+            destination = cam.ScreenToWorldPoint(new Vector3(Random.Range(screenEdgeOffset, Screen.width - screenEdgeOffset), Random.Range(screenEdgeOffset, Screen.height - screenEdgeOffset), distanceFromCamera));
+            shooting = true;
+        }
+    }
+}
